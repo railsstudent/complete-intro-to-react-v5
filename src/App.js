d9e2d2d83fbcc3f1986d395d357fc0d1f7b37254
Connie Leung <cleung334@gmail.com>
2020-07-27 00:03:42 +0800

    practice code splitting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+import { Router } from "@reach/router";
+// import Details from "./Details";
+// import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 import Navbar from './Navbar';
+
+const Details = lazy(() => import('./Details'));
+const SearchParams = lazy(() => import('./SearchParams'));
 
 const App = () => {
   const theme = useState("darkblue");
@@ -12,10 +15,12 @@ const App = () => {
     <ThemeContext.Provider value={theme}>
       <div>
         <Navbar />
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <Suspense fallback={<h1>loading route...</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );

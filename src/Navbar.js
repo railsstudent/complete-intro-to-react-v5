1b6e9002e5ae1f7dbcf32adbfc7d2e9700c38a91
Connie Leung <cleung334@gmail.com>
2020-07-26 22:52:38 +0800

    feat: design system

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from '@reach/router';
 import { css } from '@emotion/core';
+import colors from './colors';
 
 const NavBar = () => {
     return (
         <header
             css={css`
-                background-color: #333;
+                background-color: ${colors.dark};
                 position: sticky;
                 top: 0;
                 z-index: 10;
             `}
         >
-            <Link to="/">Adopt Me!</Link>
+           <Link
+                css={css`
+                    &:hover {
+                    text-decoration: underline;
+                    }
+                `}
+                to="/"
+            >Adopt Me!</Link>
             <span aria-label="logo" role="img">
                 🐩
             </span>
